86fd9549db7b9ffd04c2eaab9028dd1e2dcd6b0a
clarkwkw <clarkwkw@yahoo.com.hk>
2017-06-29 13:29:49 +0800

    add: more filling for complex fields

--- crawler/filling.py
+++ crawler/filling.py
@@ -52,18 +52,39 @@ def fill_complex(df):
 		df = func(df)
 	return df
 
-def fill_complex_accural_bal_sheet(df):
-	queue = DateQueue()
-	df["accural_bal_sheet"] = np.NAN
-	for itertuple in df.iterrows():
-		index = itertuple[0]
-		row = itertuple[1]
-		cur_date = row['date']
-		yr_prev_date = queue.pop(cur_date)
-		queue.push(cur_date)
-		if yr_prev_date is not None:
-			yr_prev_row = df.loc[yr_prev_date]
-			df.loc[index, 'accural_bal_sheet'] = row['snapshot_accurual'] - yr_prev_row['snapshot_accurual']
+def fill_complex_interval_change_factory(complex_name, src_name, interval, change_method):
+	def fill(df):
+		queue = DateQueue(interval)
+		df[complex_name] = np.NAN
+		for itertuple in df.iterrows():
+			index = itertuple[0]
+			row = itertuple[1]
+			cur_date = row['date']
+			prev_date = queue.pop(cur_date)
+			queue.push(cur_date)
+			if prev_date is not None:
+				prev_row = df.loc[prev_date]
+			if change_method == 'absolute':
+				df.loc[index, complex_name] = row[src_name] - prev_row[src_name]
+			elif change_method == 'percent':
+				df.loc[index, complex_name] = (row[src_name]/prev_row[src_name] - 1)*100
+			elif change_method == 'relative':
+				df.loc[index, complex_name] = row[src_name]/prev_row[src_name] - 1
+			else:
+				raise Exception("Invalid change_method '%s'"%change_method)
+		return df
+	return fill
+
+def fill_complex_mean_factory(complex_name, src_names, weights = None):
+	def fill(df):
+		n = float(len(src_names))
+		df[complex_name] = df[src_names[0]]/n
+		for i in range(1, len(src_names)):
+			df[complex_name] += df[src_names[i]]/n
+		return df
+
+def fill_complex_earning_mom_3m_fy1_to_fy2(df):
+	df["earning_mom_3m_fy1_to_fy2"] = df["earning_mom_3m_mean_fy1_fy2"]/df["earnings_cv_mean"]
 	return df
 
 def fill_complex_financial_health(df):
@@ -123,7 +144,25 @@ def fill_complex_financial_health(df):
 	df.drop(["ROA", "CFO", "gross_margin", "turn", "leverage", "current_ratio", "d_ROA", "d_margin", "d_leverage", "d_current_ratio", "d_turn"], axis = 1, inplace = True)
 	return df
 
-_fill_complex_funcs = [fill_complex_accural_bal_sheet, fill_complex_financial_health]
+_fill_complex_funcs = [	
+						fill_complex_interval_change_factory("accural_bal_sheet", "snapshot_accurual", 365, "absolute"),
+						fill_complex_interval_change_factory("target_price_6m_change", "best_target_price", 365, "percent"),
+						fill_complex_interval_change_factory("asset_turnover_12m_change", "asset_turnover", 365, "percent"),
+						fill_complex_interval_change_factory("earning_mom_1m_fy1", "best_eps_fy1", 30, "percent"),
+						fill_complex_interval_change_factory("earning_mom_1m_fy2", "best_eps_fy2", 30, "percent"),
+						fill_complex_mean_factory("earning_mom_1m_mean_fy1_fy2", ["earning_mom_1m_fy1", "earning_mom_1m_fy2"]),
+						fill_complex_interval_change_factory("earning_mom_3m_fy1", "best_eps_fy1", 90, "percent"),
+						fill_complex_interval_change_factory("earning_mom_3m_fy2", "best_eps_fy2", 90, "percent"),
+						fill_complex_mean_factory("earning_mom_3m_mean_fy1_fy2", ["earning_mom_3m_fy1", "earning_mom_3m_fy2"]),
+						fill_complex_mean_factory("earning_mom_mean_1m3m", ["earning_mom_1m_mean_fy1_fy2", "earning_mom_3m_mean_fy1_fy2"]),
+						fill_complex_earning_mom_3m_fy1_to_fy2,
+						fill_complex_interval_change_factory("share_out_12m_change", "total_shares", 365, "percent"),
+						fill_complex_interval_change_factory("price_accel_3m", "raw_beta", 90, "percent"),
+						fill_complex_interval_change_factory("price_accel_6m", "raw_beta", 180, "percent"),
+						fill_complex_interval_change_factory("rec_1m_change", "consenus", 30, "percent"),
+						fill_complex_interval_change_factory("rec_3m_change", "consenus", 90, "percent"),
+						fill_complex_financial_health
+					]
 
 class DateQueue:
 	def __init__(self, interval = 365):
