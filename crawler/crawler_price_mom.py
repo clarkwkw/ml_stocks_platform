91716c7e0c998d6ef10b89496899e07575d9b271
clarkwkw <clarkwkw@yahoo.com.hk>
2017-07-06 15:31:57 +0800

    update: faster crawling

--- crawler/crawler_price_mom.py
+++ crawler/crawler_price_mom.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import json
+from concurrent.futures import ThreadPoolExecutor
 import multiprocessing
-import numpy as np
 import pandas
 import schedule
 import time
@@ -15,115 +13,96 @@ host = 'seis10.se.cuhk.edu.hk'
 database = 'finanai'
 username = 'finanai'
 raw_table = 'bloomberg_raw'
+out_folder = "./historical data"
 
 email_status_dest = "clarkwkw@yahoo.com.hk"
 email_status_freq = 60
 
 periods = [(1, "price_mom_1m"), (3, "price_mom_3m"), (6, "price_mom_6m"), (12, "price_mom_12m")]
-max_thread_no = 4
+max_thread_no = 8
+sectors = list(utilities.tickers_table.keys())
 
 print_status = utilities.print_status
 cur_sector = "INIT"
-cur_stock = "INIT"
 exit_flag = False
+total_date = 0
+finished_date = 0
+errs = []
+err_mutex = multiprocessing.Lock()
 
-class CSV_Buffer:
-	def __init__(self, csv_path, limit):
-		self.file = open(csv_path, "w")
-		self.buffer_lock = multiprocessing.Lock()
-		self.content = ""
-		self.limit = limit
-		self.content_count = 0
-		self.append("date", "ticker", "field", "value")
-
-	def append(self, date, ticker, name, value):
-		new_content = "%s, %s, %s, %s\n"%(str(date), str(ticker), str(name), str(value))
-		self.buffer_lock.acquire()
-		self.content += new_content
-		self.content_count += 1
-		if self.content_count >= self.limit:
-			self.force_flush()
-		self.buffer_lock.release()
-
-	def flush(self):
-		self.buffer_lock.acquire()
-		self.force_flush()
-		self.buffer_lock.release()
-
-	def force_flush(self):
-		self.file.write(self.content)
-		self.content = ""
-		self.content_count = 0
-
-	def close(self):
-		self.force_flush()
-		self.file.close()
-
-def price_mom(ticker, end_dates, field_name, period, buffer):
+def price_mom(end_datetime, period, field_name, sector):
 	try:
-		global finished
-		for end_datetime in end_dates:
-			delta = pandas.DateOffset(months = period)
-			start_datetime = end_datetime - delta
-			end_date = end_datetime.isoformat()[0:10]
-			start_date = start_datetime.isoformat()[0:10]
-			end_date = ''.join(end_date.split('-'))
-			start_date = ''.join(start_date.split('-'))
-			result = LocalTerminal.get_reference_data(ticker, 'CUST_TRR_RETURN_HOLDING_PER', CUST_TRR_START_DT=start_date, CUST_TRR_END_DT=end_date).as_map()
-			value = result[ticker]['CUST_TRR_RETURN_HOLDING_PER']
-			buffer.append(end_date, ticker, field_name, value)
-		status_mutex.acquire()
-		print_status("\t  Crawled period %s."%period)
-		status_mutex.release()
-		buffer.flush()
+		tickers = utilities.tickers_table[sector]
+
+		delta = pandas.DateOffset(months = period)
+		start_datetime = end_datetime - delta
+		end_date = end_datetime.isoformat()[0:10]
+		start_date = start_datetime.isoformat()[0:10]
+		tmp_result = LocalTerminal.get_reference_data(tickers, 'CUST_TRR_RETURN_HOLDING_PER', CUST_TRR_START_DT=start_date, CUST_TRR_END_DT=end_date).as_map()
+		outfile = open(out_folder+"/%s-mom-%s.%s.csv"%(sector, end_date, period), "w")
+		utilities.write_row(outfile, "date", "ticker", "field", "value")
+		for ticker in tmp_result:
+			if not pandas.isnull(tmp_result[ticker]['CUST_TRR_RETURN_HOLDING_PER']):
+				utilities.write_row(outfile, end_date, ticker, field_name, tmp_result[ticker]['CUST_TRR_RETURN_HOLDING_PER'])
+		outfile.close()
 		return 0
 	except Exception as e:
 		traceback.print_exc()
-		buffer.flush()
+		err_mutex.acquire()
+		errs.append(traceback.format_exc())
+		err_mutex.release()
 		return -1
 
 def send_status_management():
 	def send_status():
 		subject = "Crawler Status Update"
-		body = "Now crawling stock [%s] of %s sector.."%(cur_stock, cur_sector)
+		body = "Crawling %s sector, progress: %d/%d."%(cur_sector, finished_date, total_date)
 		utilities.send_gmail(email_status_dest, subject, body)
+	time.sleep(10)
 	schedule.every(email_status_freq).minutes.do(send_status).run()
 	while not exit_flag:
 		schedule.run_pending()
 		time.sleep(1)
 
+def send_err_status():
+	subject = "Crawler Status Update"
+	body = "Crawler is going to terminate, error(s) occured.\n"
+	body += "BTW, was crawling %s sector, progress: %d/%d.\n"%(cur_sector, finished_date, total_date)
+	body += "Error Message:\n"
+	for errmsg in errs:
+		body += "--------------------\n"
+		body += '> ' + '\n> '.join(errmsg.rstrip("\n").split("\n"))
+	utilities.send_gmail(email_status_dest, subject, body)
+
+def send_finish_msg():
+	subject = "Crawler Status Update"
+	body = "Finished crawling, crawler is going to terminate."
+	utilities.send_gmail(email_status_dest, subject, body)
+
 mysql_conn = utilities.mysql_connection(host, database, username)
-status_mutex = multiprocessing.Lock()
-print_status("Crawling data...")
+end_dates = pandas.read_sql("SELECT DISTINCT date FROM %s ORDER BY date asc"%raw_table, mysql_conn, coerce_float = False, parse_dates = ["date"])["date"]
 
 email_thread = Thread(target = send_status_management)
 email_thread.start()
-for sector in utilities.tickers_table.keys():
+print_status("Crawling data...")
+for sector in sectors:
 	cur_sector = sector
-	print_status("\tCrawling %s sector..."%(sector))
-	buffs = {}
-	for period, _ in periods:
-		buffs[period] = CSV_Buffer(sector+"."+str(period)+".csv", 1000)
-	with ThreadPoolExecutor(max_workers = max_thread_no) as executor:
-		futures = []
-		for ticker in utilities.tickers_table[sector]:
-			cur_stock = ticker
-			print_status("\t Crawling stock %s"%ticker)
-			trading_dates = pandas.read_sql("SELECT DISTINCT date FROM %s WHERE ticker = '%s' ORDER BY date asc"%(raw_table, ticker), mysql_conn, coerce_float = False, parse_dates = ["date"])
-			trading_dates = trading_dates["date"]
-			for period, field in periods:
-				futures.append(executor.submit(price_mom, ticker, trading_dates, field, period, buffs[period]))
+	total_date = len(end_dates)
+	for end_date in end_dates:
+		with ThreadPoolExecutor(max_workers = max_thread_no) as executor:
+			futures = []
+			for period, field_name in periods:
+				futures.append(executor.submit(price_mom, end_date, period, field_name, sector))
 			for future in futures:
 				if future.result() != 0:
-					print_status("Exception occured when crawling %s, abort."%ticker)
+					print_status("Exception occured when crawling date %s, abort."%end_date)
 					exit_flag = True
 			if exit_flag:
 				break
-					
-	for sector in buffs:
-		buffs[sector].close()
-		
+		print_status("Finished crawling %s on %s"%(sector, end_date))
+		finished_date += 1
 	if exit_flag:
-		exit(-1)
-
-print_status("Done.")
+		send_err_status()
+		break
+if not exit_flag:
+	send_finish_msg()
