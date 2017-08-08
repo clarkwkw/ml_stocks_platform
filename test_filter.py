27e66e64f464055e5e0995bcc7b71cca0d3fe8cf
clarkwkw <clarkwkw@yahoo.com.hk>
2017-08-08 13:08:35 +0800

    update: testcase

--- test_filter.py
+++ test_filter.py
@@ -1,5 +1,22 @@
+import os
 import pandas
-from DataPreparation import TestingDataPreparation
+from DataPreparation import TrainingDataPreparation, TestingDataPreparation
+
+train_end_date = "2010-01-29"
+test_date = "2010-02-01"
+
+# Under ./test_output folder
+preprocessing_file = "Materials_preprocessing_info.json"
 
 df = pandas.read_csv("./test_data/Materials-test.csv", parse_dates = ['date'], na_values = ['nan'])
-test_data = TestingDataPreparation(df, True, "./test_data/Materials_preprocessing_info.json")
+train_df = df.loc[df['date'] <= pandas.Timestamp(train_end_date)]
+test_df = df.loc[df['date'] == pandas.Timestamp(test_date)]
+
+try:
+	os.makedirs("./test_output")
+except OSError:
+	pass
+os.chdir("./test_output")
+
+train_data = TrainingDataPreparation(train_df, True, 10, 20, 1, preprocessing_file)
+test_data = TestingDataPreparation(test_df, True, preprocessing_file)
