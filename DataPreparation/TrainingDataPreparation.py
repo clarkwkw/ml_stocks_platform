a5ed05fe5e2ba15546683a01872f26aead06b274
Wong Wai Chung <simonwong@pn-206-139.itsc.cuhk.edu.hk>
2017-08-10 13:44:02 +0800

    fix: StockFiltering

--- DataPreparation/TrainingDataPreparation.py
+++ DataPreparation/TrainingDataPreparation.py
@@ -5,7 +5,7 @@ from .DataPreprocessing import DataPreprocessing
 def TrainingDataPreparation(stock_data, stock_filter_flag, B_top, B_bottom, target_label_holding_period, preprocessing_file):
     #Filter the stock data
     if stock_filter_flag:
-        stock_data = StockFiltering(stock_data)
+        stock_data = StockFiltering(stock_data, 'train', preprocessing_file)
 
     #Generate target label
     stock_data = TargetLabelGeneration(stock_data, B_top, B_bottom, target_label_holding_period)
