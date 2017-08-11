05d3f25395ee08e8e74c95eefa5e54e68c627374
clarkwkw <clarkwkw@yahoo.com.hk>
2017-08-11 10:49:08 +0800

    update: LIQFilter

--- DataPreparation/Filter/LIQFilter.py
+++ DataPreparation/Filter/LIQFilter.py
@@ -15,9 +15,13 @@ def apply_parallel(dfGrouped, func):
     return result
 
 def calculate_lambda(ticker_data):
+    ticker = ticker_data.iloc[0]['ticker']
     if ticker_data.shape[0] <= 1:
         return ticker, float("-inf")
-        
+
+    # Suppress warning
+    ticker_data.is_copy = False
+    
     ticker_data["prev_price"] = np.NAN 
     ticker_data.loc[1:, "prev_price"] = ticker_data.loc[0:(ticker_data.shape[0]-1), "last_price"]
     ticker_data["net_return"] = (1.0*ticker_data["last_price"]/ticker_data["prev_price"] - 1)*100
@@ -27,12 +31,12 @@ def calculate_lambda(ticker_data):
     # modified the equation log(v(t)p(t)) in the financial paper by log(1+v(t)p(t)) to prevent overflow of small value
     ticker_data["sign_log_v_p"] *= np.log(1+ticker_data["last_price"] * ticker_data["volume"])
 
-    regression_data = ticker_data[["net_return", "sign_log_v_p"]]
-    regression_data = regression_data.drop([np.NAN, float("inf"), float("-inf")])
+    ticker_data.replace([np.inf, -np.inf], np.nan, inplace = True)
+    ticker_data.dropna(subset = ["sign_log_v_p", "net_return"], how = "any", inplace = True)
 
-    if regression_data.shape[0] > 0:
+    if ticker_data.shape[0] > 0:
         lr = LinearRegression()
-        lr.fit(regression_data["sign_log_v_p"], regression_data["net_return"])
+        lr.fit(ticker_data["sign_log_v_p"].as_matrix().reshape(-1, 1), ticker_data["net_return"].as_matrix().reshape(-1, 1))
         return ticker, lr.coef_[0]
     else:
         return ticker, float("-inf")
@@ -44,6 +48,6 @@ def LIQFilter(stock_data):
     #Sort the tickers based on lambda and select top 50%
     ticker_lambda.sort(key=lambda x:x[1], reverse=True)
 
-    filtered_stock = [ticker_lambda[i][0] for i in range(int(len(tickers)/2))]
+    filtered_stock = [ticker_lambda[i][0] for i in range(int(len(ticker_lambda)/2))]
     
     return stock_data.loc[stock_data['ticker'].isin(filtered_stock)]
