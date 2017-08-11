2aac11d6c500d59165f1aab4ec001f19c3c61946
clarkwkw <clarkwkw@yahoo.com.hk>
2017-08-11 13:06:13 +0800

    update: revert to old filter

--- DataPreparation/Filter/LIQFilter.py
+++ DataPreparation/Filter/LIQFilter.py
@@ -1,53 +1,75 @@
 import pandas as pd
 import numpy as np
+import math as m
+from tqdm import tqdm
 from sklearn.linear_model import LinearRegression
-import config
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
-def apply_parallel(dfGrouped, func):
-    result = []
-    with ThreadPoolExecutor(max_workers = config.max_thread) as executor:
-        futures = []
-        for _, group in dfGrouped:
-            futures.append(executor.submit(func, group))
-        for future in as_completed(futures):
-            result.append(future.result())
-    return result
+from multiprocessing import Pool as ThreadPool
 
-def calculate_lambda(ticker_data):
-    ticker = ticker_data.iloc[0]['ticker']
-    if ticker_data.shape[0] <= 1:
-        return ticker, float("-inf")
+def calculate_r_log_vt_pt(t):
+    data = t[1]
 
-    # Suppress warning
-    ticker_data.is_copy = False
+    data.sort_values(by=['date'],inplace=True)
 
-    ticker_data["prev_price"] = np.NAN 
-    ticker_data["prev_price"].iloc[1:] = ticker_data["last_price"].iloc[0:(ticker_data.shape[0]-1)]
-    ticker_data["net_return"] = (1.0*ticker_data["last_price"]/ticker_data["prev_price"] - 1)*100
-    ticker_data["sign_log_v_p"] = 1
-    ticker_data.loc[ticker_data["net_return"] < 0, "sign_log_v_p"] = -1
+    t_len = data.shape[0]
+    r_t = [float('nan')]
+    log_v_p = [float('nan')]
 
-    # modified the equation log(v(t)p(t)) in the financial paper by log(1+v(t)p(t)) to prevent overflow of small value
-    ticker_data["sign_log_v_p"] *= np.log(1+ticker_data["last_price"] * ticker_data["volume"])
+    for i in range(1,t_len):
+        r = (1.0*data.iloc[i].last_price/data.iloc[i-1].last_price - 1) * 100
+        if pd.isnull(r) or r == float("inf") or r == float("-inf"):
+            continue
+        if r >= 0:
+            sign = 1
+        else:
+            sign = -1
+        r_t.append(r)
+        #modified the equation log(v(t)p(t)) in the financial paper by log(1+v(t)p(t)) to prevent overflow of small value
+        log_v_p.append(sign*np.log(1+data.iloc[i].last_price*data.iloc[i].volume))
+    data['net_return'] = r_t
+    data['sign_log_v_p'] = log_v_p
 
-    ticker_data.replace([np.inf, -np.inf], np.nan, inplace = True)
-    ticker_data.dropna(subset = ["sign_log_v_p", "net_return"], how = "any", inplace = True)
-
-    if ticker_data.shape[0] > 0:
-        lr = LinearRegression()
-        lr.fit(ticker_data["sign_log_v_p"].as_matrix().reshape(-1, 1), ticker_data["net_return"].as_matrix().reshape(-1, 1))
-        return ticker, lr.coef_[0]
-    else:
-        return ticker, float("-inf")
+    return t[0],data
 
 def LIQFilter(stock_data):
 
-    ticker_lambda = apply_parallel(stock_data.groupby(['ticker']), calculate_lambda)
+    #Split the stock data by ticker
+    tickers=stock_data['ticker'].unique()
+
+    ticker_data = {t : pd.DataFrame for t in tickers}
+
+    for key in ticker_data.keys():
+        ticker_data[key] = stock_data[:][stock_data.ticker == key] 
+
+    #Calculate the r(t) and log v(t)p(t) of each stock
+    t = [(key,ticker_data[key]) for key in ticker_data.keys()]
+
+    pool = ThreadPool() 
+
+    pool_outputs = pool.map(calculate_r_log_vt_pt, t)
+    pool.close()
+    pool.join()
+    
+    for i in pool_outputs:
+        ticker_data[i[0]] = i[1]      
+   
+    #Regress lambda of each stock using linear regression
+    ticker_lambda = []
+
+    for ticker in tickers:
+        t = ticker_data[ticker]
+        y_train = t['net_return'][1:].as_matrix()
+        x_train = t['sign_log_v_p'][1:].as_matrix().reshape(-1, 1)
+
+        #Build the regressor
+        if y_train.shape[0] >= 1:
+            lr = LinearRegression()
+            lr.fit(x_train,y_train)
+            ticker_lambda.append([ticker,lr.coef_[0]])
         
     #Sort the tickers based on lambda and select top 50%
     ticker_lambda.sort(key=lambda x:x[1], reverse=True)
 
-    filtered_stock = [ticker_lambda[i][0] for i in range(int(len(ticker_lambda)/2))]
+    filtered_stock = [ticker_lambda[i][0] for i in range(int(len(tickers)/2))]
     
     return stock_data.loc[stock_data['ticker'].isin(filtered_stock)]
