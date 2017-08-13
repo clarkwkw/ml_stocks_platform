da4894598e4dc0d6d3cd5e6785cbd264a095af5e
clarkwkw <clarkwkw@yahoo.com.hk>
2017-08-13 18:10:14 +0800

    update: build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 defaultpyint=$(which python)
 read -p "Python interpreter [default=$defaultpyint]: " pyint
 pyint=${pyint:=$defaultpyint}
-zip -rq mlsp.zip DataPreparation MachineLearningModelUtilities PortfolioConstructionUtilities __main__.py config.py debug.py
+zip -rq mlsp.zip ConfigFilesUtilities DataPreparation MachineLearningModelUtilities PortfolioConstructionUtilities __main__.py config.py debug.py
 echo "#!$pyint" | cat - mlsp.zip > mlsp
 rm mlsp.zip
 chmod +x mlsp
