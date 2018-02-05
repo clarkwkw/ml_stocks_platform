6c11afe4b4e14dacad916ef6fd3f56ce1a4ffbfc
clarkwkw <clarkwkw@yahoo.com.hk>
2018-02-05 10:40:22 +0800

    enhance: uploader

--- crawler/uploader.py
+++ crawler/uploader.py
@@ -1,12 +1,43 @@
 import pandas
-from utilities import print_status, mysql_connection
+from utilities import print_status, mysql_connection, tickers_table
+import os
 
 host = 'seis10.se.cuhk.edu.hk'
 database = 'finanai'
 username = 'finanai'
 
-target_table = 'US_bloomberg_factor_2'
-filelist = [('./historical data/Materials.csv', 'Materials'), ('./historical data/Energy.csv', 'Energy')]
+target_table = 'HK2_bloomberg_factor'
+file_dir = "./historical data"
+filelist = []
+err_files = {
+	"format": [],
+	"sector": []
+}
+
+tmplist = os.listdir(file_dir)
+for path in tmplist:
+	basename = os.path.basename(path)
+	name, ext = os.path.splitext(basename)
+	if ext.lower() == ".csv":
+		try:
+			sector, _ = name.split("-", 1)
+			if sector not in tickers_table:
+				err_files["sector"].append(basename)
+			else:
+				filelist.append((file_dir.rstrip("/") + "/" + path, sector))
+		except ValueError:
+			err_files["format"].append(basename)
+
+for reason, files in err_files.items():
+	if len(files) > 0:
+		print("Files including:")
+		print("\t" + "\n\t".join(files))
+		print("   are not included due to %s error."%reason)
+		response = ""
+		while response.lower() not in ["y", "n"]:
+			response = raw_input("Continue? [y/n] ")
+		if response.lower() == "n":
+			exit(-1)
 
 mysql_conn = mysql_connection(host, database, username)
 for (filepath, sector) in filelist:
